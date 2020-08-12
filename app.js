f77410743af7be93bded0bfab1180cd8d79bf98f
sofyann84 <sofyan.dbx@gmail.com>
2020-08-12 22:09:46 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+const mongoose = require('mongoose');
+
+var indexRouter = require('./routes/index');
+var phonebooksRouter = require('./routes/phonebooks');
+
+mongoose.connect('mongodb://localhost/cha22', {useNewUrlParser: true});
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('api/phonebooks', phonebooksRouter);
+
+module.exports = app;
---
+++ models/phonebook.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const phonebookSchema = new mongoose.Schema({
+    name: String,
+    phone: String
+  });
+
+  module.exports = mongoose.model('Phonebook', phonebookSchema);
+
+
